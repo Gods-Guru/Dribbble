9c52b009c71ac36633bd35e97f2f77d49aaddc08
Gods-Guru <ekonduobe@gmail.com>
2024-09-23 15:47:59 +0100

    Update Box.jsx

--- src/Box.jsx
+++ src/Box.jsx
@@ -9,7 +9,7 @@ export default function Box(){
         <>
         <section className="head">
             <div className="box">Over 3 million ready to work creatives!</div>
-            <p className="text">The world's destination for design.</p>
+            <p className="text">The world's destination for design.☠️☠️☠️☠️☠️</p>
             <p className="body">Get inspired by the work of millions of top-rated designers & agencies around the world.</p>
             <button className="start">Get Started</button>
         </section>
